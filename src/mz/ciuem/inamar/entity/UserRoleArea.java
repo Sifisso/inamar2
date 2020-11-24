0447e5b1032434b24ada12200c85db5c69dd218e
Sifisso <sifissom@gmail.com>
2020-11-24 15:19:11 +0200

    Configuracao dos UserroleArea

---
+++ src/mz/ciuem/inamar/entity/UserRoleArea.java
@@ -0,0 +1,57 @@
+package mz.ciuem.inamar.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_role_area")
+public class UserRoleArea extends IdEntity{
+
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userRole_id", insertable = true, updatable = true)
+	private UserRole userRole;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "area_id", insertable = true, updatable = true)
+	private Area area;
+	
+	
+	private String descricao;
+
+
+	public UserRole getUserRole() {
+		return userRole;
+	}
+
+
+	public void setUserRole(UserRole userRole) {
+		this.userRole = userRole;
+	}
+
+
+	public Area getArea() {
+		return area;
+	}
+
+
+	public void setArea(Area area) {
+		this.area = area;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
